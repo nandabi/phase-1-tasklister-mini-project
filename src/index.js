abde31e159f050b1e6270633170f11534055ada8
nandabi <valerie.nandabi@student.moringaschool.com>
2024-01-12 12:14:58 +0300

    commit message

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+  const submit = document.querySelector('form')
+  const inputValue = document.querySelector('#new-task-description')
+  const ul = document.querySelector('#tasks')
+
+  function addTask (){
+    const li =document.createElement('li')
+    li.textContent=inputValue.value;
+    ul.appendChild(li)
+
+    function removeTask(){
+      const removeBtn = document.createElement('button')
+      removeBtn.textcontent = 'x'
+      li.append(removeBtn);
+      removeBtn.addEventListener('click',(e)=>{
+        e.target.parentNode.remove();
+      })
+    }
+    function selectpriority(){
+      const dropdown=document.createElement('select')
+      dropdown.innerHTML = optionvalue = "high" 
+
+    }
+ selectpriority()
+ removeTask
+    }
+
+submit.addEventListener('submit',function(e){
+  e.preventDefault();
+  addTask()
+})
+})
